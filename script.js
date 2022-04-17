d5dc5cec69096e593fc7747ab97329f095c3ff51
brendan <brendanjef@gmail.com>
2022-04-16 23:24:26 -0700

    Create function 'computerPlay'

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function computerPlay () {
+function computerPlay() {
+    const roshambo = ["Rock", "Paper", "Scissors"];
 
-    
+    const random = Math.floor(Math.random() * roshambo.length);
+
+    return roshambo[random];
+}
+
+console.log(computerPlay());
+
+function playGame() {
+    let playerSelection = prompt("Let's play a game! Pick rock, paper or scissors!");
+
+
 }
