c64d97293f9cd7f5aa7dfbe884bb904f3bc7fbbc
brendan <brendanjef@gmail.com>
2022-04-19 11:48:02 -0700

    Add playRound function

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 function computerPlay() {
-    const roshambo = ["Rock", "Paper", "Scissors"];
+    const roshambo = ["rock", "paper", "scissors"];
 
     const random = Math.floor(Math.random() * roshambo.length);
 
     return roshambo[random];
 }
 
-console.log(computerPlay());
+const playerSelection = prompt("Let's playa game! Pick rock, paper or scissors.").toLowerCase();
+console.log(playerSelection)
 
-function playGame() {
-    let playerSelection = prompt("Let's play a game! Pick rock, paper or scissors!");
+const computerSelection = computerPlay();
+console.log(computerSelection);
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie.";
+    }
+    if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            return "You Win";
+        } else {
+            return "You Lose";
+        }
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You Win";
+        } else {
+            return "You Lose";
+        }
+    }
+    else if(playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You Win";
+        } else {
+            return "You Lose";
+        }
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+
+    }
 
 }
